14df6c3399bf65adf41a316d0a5cf9893df1951a
David Stevenson <drstevenson1958@yahoo.com>
2024-04-15 13:20:34 -0400

    Activity 2.1: Creating a React App to present yourself

---
+++ ch02/react-projects/me/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1>Hi, this is me - Dave!</h1>
+      <p>Right now, I am 65 years old, and I live in Henrietta, NY.</p>
+      <p>My full name is David R. Stevenson and I am a web developer.</p>
+    </div>
+  );
+}
+
+export default App;
