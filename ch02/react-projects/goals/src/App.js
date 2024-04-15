3dd2798fbb148a7200906da9765fee27be4ce8ac
David Stevenson <drstevenson1958@yahoo.com>
2024-04-15 13:49:37 -0400

    Activity 2.2 Creating a React App to Log Your Goals For This Book
    Also provided screen shots of React Apps for Hi, This is me and Goal List projects.

---
+++ ch02/react-projects/goals/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './components/Header.js' ;
+import GoalList from './components/GoalList.js' ;
+
+function App() {
+  return (
+    <>
+      <Header />
+      <GoalList />
+    </>
+  );
+}
+
+export default App;
