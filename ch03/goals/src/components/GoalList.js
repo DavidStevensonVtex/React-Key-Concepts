df4ef2d121d2a8cb83ba033bc17f72e2646da7e2
David Stevenson <drstevenson1958@yahoo.com>
2024-04-16 11:17:24 -0400

    Implement second goal in GoalList component.
    Remove imports of FirstGoal and SecondGoal components.

--- ch03/goals/src/components/GoalList.js
+++ ch03/goals/src/components/GoalList.js
@@ -1,6 +1,4 @@
 import Goal from './Goal.js' ;
-import FirstGoal from './FirstGoal.js' ;
-import SecondGoal from './SecondGoal.js' ;
 import ThirdGoal from './ThirdGoal.js' ;
 
 function GoalList() {
@@ -9,8 +7,9 @@ function GoalList() {
           <Goal id="g1" title="Learn React (without TypeScript)">
             <p>I want to read the book <i>React Key Concepts</i> by Maximilian Schwarzmüller.</p>
           </Goal>
-          <FirstGoal />
-          <SecondGoal />
+          <Goal id="g2" title="I want to learn by doing.">
+            <p>I will try to do the practice exercises, as learning by doing is the best way to learn.</p>
+          </Goal>
           <ThirdGoal />
         </ul>
       );
