9231913dfc84990f0e4b232b5b293b9e4d20c8c9
David Stevenson <drstevenson1958@yahoo.com>
2024-04-18 10:55:50 -0400

    Chapter 4: Working with Events and State

---
+++ ch04/EmailInput.js
@@ -0,0 +1,9 @@
+function EmailInput()
+{
+    return {
+        <div>
+            <input placeholder="Your email" type="email" />
+            <p>The entered email address is invalid</p>
+        </div>
+    } ;
+}
