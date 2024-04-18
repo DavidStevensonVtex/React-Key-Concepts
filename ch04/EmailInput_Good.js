3e26603354cde240cf854814f068aab26c807b36
David Stevenson <drstevenson1958@yahoo.com>
2024-04-18 11:04:09 -0400

    Properly reacting to events

---
+++ ch04/EmailInput_Good.js
@@ -0,0 +1,19 @@
+function EmailInput()
+{
+    let errorMessage = '' ;
+    
+    function evaluateEmail(event) {
+        const enteredEmail = event.target.value ;
+        if (enteredEmail.trim() == '' || !enteredEmail.includes('@')) {
+            errorMessage = 'The entered email address is invalid.' ;
+        } else { 
+            errorMessage = '' ;
+        }
+    }
+    return {
+        <div>
+            <input placeholder="Your email" type="email" />
+            <p>The entered email address is invalid</p>
+        </div>
+    } ;
+}
