4435dbd8b628f23b2d19818ba33bd2e3dba7deb8
David Stevenson <drstevenson1958@yahoo.com>
2024-04-18 15:16:21 -0400

    Updating State Based on Previous State Correctly
    This uses a function passed to the set function. React calls the function supplied, passing the latest state value to the supplied function.

--- ch04/Counter.js
+++ ch04/Counter.js
@@ -5,7 +5,9 @@ function Counter()
     const [counter, setCounter] = useState(0);
 
     function incrementCounterHandler() {
-        setCounter(counter + 1);
+        setCounter(function(prevCounter) { return prevCounter + 1; })
+        // alternatively, JS arrow functions could be used:
+        // setCounter(prevCounter => prevCounter + 1);
     }
 
     return (
