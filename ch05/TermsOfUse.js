37b6abb87ad1049ffa430cffc59afa5fb0f53c79
David Stevenson <drstevenson1958@yahoo.com>
2024-04-19 13:10:34 -0400

    Chapter 5: Rendering Lists and Conditional Content

---
+++ ch05/TermsOfUse.js
@@ -0,0 +1,13 @@
+import { useState } from 'react' ;
+
+function TErmsOfUse() {
+    return (
+        <section>
+            <button>Show Terms of Use Summary</button>
+            <p>
+                By continuing, you accept that we will not indemnify you 
+                for any damage or harm caused by our products.
+            </p>
+        </section>
+    )
+}
