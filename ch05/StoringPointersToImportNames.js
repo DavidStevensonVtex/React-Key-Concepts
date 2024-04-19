d1c58437e1bcf1bd871b781a5d30986cf8bc8b47
David Stevenson <drstevenson1958@yahoo.com>
2024-04-19 13:51:03 -0400

    Storing Pointers to Imported Custom Component Names

---
+++ ch05/StoringPointersToImportNames.js
@@ -0,0 +1,6 @@
+import MyComponent from './my-component' ;
+import MyOtherComponent from './my-other-component' ;
+
+let someCondition = false ;
+
+const tag = someCondition ? MyComponent : MyOtherComponent ;
