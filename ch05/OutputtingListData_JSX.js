d90f282fc847a64f6fa212064274c4dedd8a45c3
David Stevenson <drstevenson1958@yahoo.com>
2024-04-19 14:07:52 -0400

    Outputting List Data with JSX

---
+++ ch05/OutputtingListData_JSX.js
@@ -0,0 +1,18 @@
+import { useState } from 'react' ;
+
+const products = [
+    { id: 'p1', title: 'A Book', price: 59.99 },
+    { id: 'p2', title: 'A Carpet', price: 129.49 },
+    { id: 'p3', title: 'Another Book', price: 39.99 }
+] ;
+
+const transformedProducts = [] ;
+
+for ( const product of products) {
+    transformedProducts.push((
+        <li>
+            <h2>{product.title}</h2>
+            <p>${product.price}</p>
+        </li>
+    ));
+}
