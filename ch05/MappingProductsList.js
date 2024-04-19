8a856db9d415baaada3a0b1cd3697d99f3bf33c6
David Stevenson <drstevenson1958@yahoo.com>
2024-04-19 14:22:11 -0400

    Mapping List Data: ProductsList React component

---
+++ ch05/MappingProductsList.js
@@ -0,0 +1,17 @@
+import { useState } from 'react' ;
+
+function ProductsList( { products } )
+{
+    const productElements = products.map( product => (
+        <li>
+            <h2>{product.title}</h2>
+            <p>${product.price}</p>
+        </li>
+    ));
+
+    return (
+        <ul>
+            {productElements}
+        </ul>
+    ) ;
+}
