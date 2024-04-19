9c7b63510f8ecc851f405f3b71b12096a996c2f7
David Stevenson <drstevenson1958@yahoo.com>
2024-04-19 13:27:01 -0400

    Using ternary expression involving JSX inside curly braces.

--- ch05/TermsOfUse.js
+++ ch05/TermsOfUse.js
@@ -7,19 +7,18 @@ function TermsOfUse() {
         setShowTerms(true);
     }
 
-    const paragraph = showTerms ? 
-        ( 
-            <p>
-                By continuing, you accept that we will not indemnify you 
-                for any damage or harm caused by our products.
-            </p>
-        ) :
-        null ;
-
     return (
         <section>
             <button onClick={showTermsSummaryHandler}>Show Terms of Use Summary</button>
-            {paragraph}
+            { showTerms ? 
+                ( 
+                    <p>
+                        By continuing, you accept that we will not indemnify you 
+                        for any damage or harm caused by our products.
+                    </p>
+                ) :
+                null
+            }
         </section>
     )
 }
