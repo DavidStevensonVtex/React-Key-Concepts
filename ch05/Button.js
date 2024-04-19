438701a36d60e1a14c5f9c1a9bfb97becba00a60
David Stevenson <drstevenson1958@yahoo.com>
2024-04-19 13:47:26 -0400

    Assigning element tag name to constant and using in React JSX expression

--- ch05/Button.js
+++ ch05/Button.js
@@ -1,11 +1,6 @@
 import { useState } from 'react' ;
 
 function Button ( { isButton, config, children } ) {
-    if (isButton) {
-        if (isButton) {
-            return <button {...config}>{children}</button> ;
-        }
-    }
-
-    return <a {...config}>{children}</a>
+    const Tag = isButton ? 'button' : 'a' ;
+    return <Tag {...config}>{children}</Tag> ;
 }
