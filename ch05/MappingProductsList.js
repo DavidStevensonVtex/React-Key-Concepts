e92937ce76de1c31732a9102814d7e9d3c324256
David Stevenson <drstevenson1958@yahoo.com>
2024-04-20 19:18:40 -0400

    Mapping List Data: Shorter ProductsList React component

--- ch05/MappingProductsList.js
+++ ch05/MappingProductsList.js
@@ -2,13 +2,6 @@ import { useState } from 'react' ;
 
 function ProductsList( { products } )
 {
-    const productElements = products.map( product => (
-        <li>
-            <h2>{product.title}</h2>
-            <p>${product.price}</p>
-        </li>
-    ));
-
     return (
         <ul>
             {   products.map(product => (
