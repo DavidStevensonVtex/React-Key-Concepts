22da3b1a52b1c78315f61b15136f1f8714d40126
David Stevenson <drstevenson1958@yahoo.com>
2024-04-21 16:24:54 -0400

    Activity 5.2: Outputting a List of Products, Implement Add Product button

--- ch05/examples/outputproducts/src/components/ProductList.js
+++ ch05/examples/outputproducts/src/components/ProductList.js
@@ -1,17 +1,31 @@
 import { useState } from 'react';
 
-let products = [ 
+let initialProducts = [ 
     { id: 1, name: 'React - The Complete Guide', productType: 'Course', price: 19.99 },
     { id: 2, name: 'Stylish Chair', price: 329.49 },
     { id: 3, name: 'Ergonomic Chair', price: 269.99 },
     { id: 4, name: 'History Video Game Collection', price: 99.99 }
 ];
 
+let productAdded = false ;
+
 function ProductList()
 {
+    const [products, setProducts] = useState(initialProducts) ;
+
+    function addProduct() {
+        const newProduct = { id: 5, name: 'Another new product', price: 15.99 } ;
+
+        if (! productAdded ) {
+            setProducts( [...products, newProduct] ) ;
+            productAdded = true ;
+        }
+
+    }
+
     return (
         <>
-            <button type='button'>Add Product</button>
+            <button type='button' onClick={addProduct}>Add Product</button>
             { products.map( (p) => (
                 <div key={'p' + p.id} className='product-list'>      
                     {p.name}&nbsp;{p.productType && '[' + p.productType + '] '}
