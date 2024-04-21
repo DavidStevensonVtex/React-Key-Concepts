f9f35aeba296f1d03a5c4636c5f3934778a11ad9
David Stevenson <drstevenson1958@yahoo.com>
2024-04-21 15:13:04 -0400

    Activity 5.1 Showing a Conditional Error Message
    Validate email

---
+++ ch05/examples/validateemail/src/components/Email.js
@@ -0,0 +1,12 @@
+import { useState } from 'react' ;
+
+function Email()
+{
+    return (
+        <>
+            <h2>Your email</h2>
+            <input type='text' /><br />
+            <button type='button'>Submit</button>
+        </>
+    ) ; 
+}
