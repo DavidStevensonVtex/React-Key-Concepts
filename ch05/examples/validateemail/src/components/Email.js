f30e3191bc567938a45cb3caff37cdb7e8c390e1
David Stevenson <drstevenson1958@yahoo.com>
2024-04-21 15:42:39 -0400

    Activity 5.1: Showing a Conditional Error Message: Validate Email

--- ch05/examples/validateemail/src/components/Email.js
+++ ch05/examples/validateemail/src/components/Email.js
@@ -1,11 +1,31 @@
+import { useState } from 'react' ;
+
 function Email()
 {
+    const [email, setEmail] = useState('');
+    const [validateEmail, setValidateEmail] = useState(false);
+    let validationMessage = <span /> ;
+
+    function emailChanged(event)
+    {
+        setEmail(event.target.value);
+    }
+
+    function buttonClicked()
+    {
+        setValidateEmail(true);
+    }
+
+    if ( validateEmail && !email.includes('@')) {
+        validationMessage = <p>Invalid email address entered!</p> ;
+    }
 
     return (
         <>
             <h2>Your email</h2>
-            <input type='text' /><br />
-            <button type='button'>Submit</button>
+            <input type='text' onBlur={emailChanged}/><br />
+            <button type='button' onClick={buttonClicked}>Submit</button>
+            {validationMessage}
         </>
     ) ; 
 }
