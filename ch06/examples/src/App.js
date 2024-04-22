d1f0066d2ce21cbfb561ba2f39940eea247c4f0b
David Stevenson <drstevenson1958@yahoo.com>
2024-04-22 09:20:15 -0400

    Setting Styles Dynamically: TodoPriority: Using className property and dynamic class name

--- ch06/examples/src/App.js
+++ ch06/examples/src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 // import SettingStylesDynamically from './components/SettingStylesDynamically'
 // import SelectivelyApplyingStyles from './components/SelectivelyApplyingStyles' ;
-import SettingInLineStyles from './components/SettingInLineStyles' ;
+// import SettingInLineStyles from './components/SettingInLineStyles' ;
+import TodoPriority from './components/TodoPriority' ;
 
 function App() {
-  //     <SettingStylesDynamically />
+  // <SettingStylesDynamically />
   // <SelectivelyApplyingStyles />
+  // <SettingInLineStyles />
   return (
-    <SettingInLineStyles />
+    <TodoPriority />
   );
 }
 
