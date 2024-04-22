e4e6d391b852fcf76b950698edd8703e0e9fe7df
David Stevenson <drstevenson1958@yahoo.com>
2024-04-22 11:17:00 -0400

    Scoped Styles with CSS Modules

--- ch06/examples/src/components/TextBox.js
+++ ch06/examples/src/components/TextBox.js
@@ -1,14 +1,14 @@
 // Customization with Fixed Configuration Options
-import './TextBox.css' ;
+import classes from './TextBox.module.css' ;
 
 function TextBox( { children, mode } ) {
     let cssClasses ;
 
     if (mode === 'alert' ) {
-        cssClasses = 'box-alert' ;
+        cssClasses = classes.alert ;
     }
     else if ( mode === 'info' ) {
-        cssClasses = 'box-info' ;
+        cssClasses = classes.info ;
     }
 
     return <p className={cssClasses}>{children}</p>
