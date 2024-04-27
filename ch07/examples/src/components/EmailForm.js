8db02c87d11c96ce537cb05d10839ceeab4900e5
David Stevenson <drstevenson1958@yahoo.com>
2024-04-27 09:59:13 -0400

    Using references instead of state (useRef)

--- ch07/examples/src/components/EmailForm.js
+++ ch07/examples/src/components/EmailForm.js
@@ -1,22 +1,22 @@
-import { useState } from 'react' ;
+import { useRef } from 'react' ;
 import classes from "./EmailForm.css" ;
 
 export default function EmailForm() {
-    const [enteredEmail, setEnteredEmail] = useState('');
-
-    function UpdateEmailHandler(event) {
-        setEnteredEmail ( event.target.value );
-    }
+    const emailRef = useRef();
 
     function submitFormHandler(event) {
         // could send entered email to backend server
-        console.log("enteredEmail", enteredEmail);
+        event.preventDefault();
+        console.log("emailRef", emailRef.current.value);
     }
 
     return (
         <form className={classes.form} onSubmit={submitFormHandler}>
             <div><label htmlFor='email'>Your email</label></div>
-            <div><input type="email" id="email" onChange={UpdateEmailHandler} /></div>
+            <div><input 
+                ref={emailRef}
+                type="email" 
+                id="email"/></div>
             <button>Save</button>
         </form>
     )
