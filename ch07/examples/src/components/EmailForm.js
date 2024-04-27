ecebe477c6450d0757e8989b5de4c18d2a8ebbec
David Stevenson <drstevenson1958@yahoo.com>
2024-04-27 10:20:29 -0400

    Refs versus State

--- ch07/examples/src/components/EmailForm.js
+++ ch07/examples/src/components/EmailForm.js
@@ -1,22 +1,37 @@
-import { useRef } from 'react' ;
+import { useState } from 'react' ;
 import classes from "./EmailForm.css" ;
 
+// Should you always use refs instead of state?
+// The clear answer to this question is "no".
+
+// As soon as you need to change values and those changes must be relfected
+// in the UI (for example, by rendering some conditional content), refs are
+// out of the game.
+
 export default function EmailForm() {
-    const emailRef = useRef();
+    const [enteredEmail, setEnteredEmail] = useState('');
+
+    function UpdateEmailHandler(event) {
+        setEnteredEmail ( event.target.value );
+    }
 
     function submitFormHandler(event) {
         // could send entered email to backend server
         event.preventDefault();
-        console.log("emailRef", emailRef.current.value);
+        console.log("enteredEmail", enteredEmail);
+        setEnteredEmail('');
     }
 
     return (
         <form className={classes.form} onSubmit={submitFormHandler}>
             <div><label htmlFor='email'>Your email</label></div>
-            <div><input 
-                ref={emailRef}
+            <div>
+                <input 
                 type="email" 
-                id="email"/></div>
+                id="email" 
+                onChange={UpdateEmailHandler}
+                value={enteredEmail} />
+            </div>
             <button>Save</button>
         </form>
     )
