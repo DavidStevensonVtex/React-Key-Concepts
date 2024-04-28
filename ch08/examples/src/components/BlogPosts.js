9fbd6d889630808252b87f8581182a9b45c3a8c8
David Stevenson <drstevenson1958@yahoo.com>
2024-04-28 19:32:24 -0400

    Fixing the infinite recursion problem, but loading data shouldn't require user interaction

--- ch08/examples/src/components/BlogPosts.js
+++ ch08/examples/src/components/BlogPosts.js
@@ -3,7 +3,8 @@ import { useState } from 'react' ;
 import classes from './BlogPosts.module.css' ;
 
 async function fetchPosts() {
-    const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+    // const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const response = await fetch('posts.json');
     const blogPosts = await response.json();
     return blogPosts;
 }
@@ -12,18 +13,21 @@ function BlogPosts() {
     const [loadedPosts, setLoadedPosts] = useState([]);
 
     function fetchPostsHandler() {
-        fetchPosts().then( (fetchedPosts) => setLoadedPosts(fetchPosts)) ;
+        fetchPosts().then( (fetchedPosts) => { setLoadedPosts(fetchedPosts) }) ;
     }
 
     return (
         <>
             <button onClick={fetchPostsHandler}>Fetch Posts</button>
             <ul className={classes.posts}>
-                { loadedPosts.map( (post) => (
-                    <li key={post.id}>{ post.title }</li>
-                ))}
+                {
+                    loadedPosts.map( post => (
+                        <li key={post.id}>{ post.title }</li>
+                    ))
+                }
             </ul>
         </>
-
     )
 }
+
+export default BlogPosts ;
