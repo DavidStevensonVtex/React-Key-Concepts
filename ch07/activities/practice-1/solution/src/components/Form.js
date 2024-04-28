fdf9e6272f8f3603c645b3e761ab265be233124b
David Stevenson <drstevenson1958@yahoo.com>
2024-04-28 16:14:01 -0400

    Activity 7.1: Extract User Input Values, Solution

--- ch07/activities/practice-1/solution/src/components/Form.js
+++ ch07/activities/practice-1/solution/src/components/Form.js
@@ -1,17 +1,26 @@
+import { useRef } from 'react' ;
+
 function Form() {
+  const nameRef = useRef();
+  const programRef = useRef();
+
   function formSubmitHandler(event) {
     event.preventDefault();
+    const name = nameRef.current.value;
+    const program = programRef.current.value ;
+    console.log("name", name, "program", program);
+    // console.log(programRef.current);
   }
 
   return (
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
         <label htmlFor="name">Your name</label>
-        <input type="text" id="name" />
+        <input type="text" id="name" ref={nameRef} />
       </div>
       <div className="form-control">
         <label htmlFor="program">Choose your program</label>
-        <select id="program">
+        <select id="program" ref={programRef}>
           <option value="basics">The Basics</option>
           <option value="advanced">Advanced Concepts</option>
           <option value="mastery">Mastery</option>
