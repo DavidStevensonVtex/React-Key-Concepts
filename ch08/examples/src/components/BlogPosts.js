e9683d4383a572a1407a30adeb8215abc93fb6ca
David Stevenson <drstevenson1958@yahoo.com>
2024-04-28 20:00:05 -0400

    Effects and Their Dependencies

--- ch08/examples/src/components/BlogPosts.js
+++ ch08/examples/src/components/BlogPosts.js
@@ -2,22 +2,31 @@ import { useState, useEffect } from 'react' ;
 
 import classes from './BlogPosts.module.css' ;
 
-async function fetchPosts() {
-    const response = await fetch('https://jsonplaceholder.typicode.com/posts');
-    // const response = await fetch('posts.json');
+const DEFAULT_URL = "https://jsonplaceholder.typicode.com/posts" ;
+// const DEFAULT_URL = "posts.json" ;
+
+async function fetchPosts(url) {
+    const response = await fetch(url);
     const blogPosts = await response.json();
     return blogPosts;
 }
 
 function BlogPosts() {
+    const [postsUrl, setPostsUrl] = useState(DEFAULT_URL);
     const [loadedPosts, setLoadedPosts] = useState([]);
-
+ 
     useEffect(function () {
-        fetchPosts().then( (fetchedPosts) => { setLoadedPosts(fetchedPosts) }) ;
-    });
+        fetchPosts(postsUrl).then( (fetchedPosts) => { setLoadedPosts(fetchedPosts) }) ;
+    }, [postsUrl]);
+
+    function adjustUrlHandler(event) {
+        console.log("url", event.target.value);
+        setPostsUrl(event.target.value);
+    }
 
     return (
         <>
+            <input className={classes.input} type="text" onBlur={adjustUrlHandler} />
             <ul className={classes.posts}>
                 {
                     loadedPosts.map( post => (
