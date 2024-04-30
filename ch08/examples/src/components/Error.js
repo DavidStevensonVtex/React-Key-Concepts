a398ec1b7e1f9c0451744c5432116d3ced7504ea
David Stevenson <drsgis@rit.edu>
2024-04-30 11:11:07 -0400

    Destructuring the props object was supposed to fix the unnecessary executions, but didn't appear to work

--- ch08/examples/src/components/Error.js
+++ ch08/examples/src/components/Error.js
@@ -3,16 +3,19 @@ import { useEffect } from 'react' ;
 // Another common reason for unnecessary effect execution is 
 // the usage of objects as dependencies.
 
-export default function Error(props) {
+export default function Error({ message }) {
+    // const { message } = props ;
+
     useEffect(
         function () {
             // performing some error logging
             // in a real app, a HTTP request might be sent to some analytics API
             console.log('An error occurred!');
-            console.log(props.message);
+            console.log(message);
         },
-        [props]
+        // [props] don't use the entire props object!
+        [message]
     )
 
-    return <p>{props.message}</p>
+    return <p>{message}</p>
 }
