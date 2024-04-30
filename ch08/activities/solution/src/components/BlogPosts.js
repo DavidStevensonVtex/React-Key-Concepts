29a729cfea59be1f19b66750cbad79c61522d49d
David Stevenson <drsgis@rit.edu>
2024-04-30 13:20:48 -0400

    Activity 8.1 Building a Basic Blog: 1. Send a GET HTTP Request to the Web API to fetch blog posts.

--- ch08/activities/solution/src/components/BlogPosts.js
+++ ch08/activities/solution/src/components/BlogPosts.js
@@ -1,7 +1,40 @@
+import { useState, useEffect } from 'react' ;
 import classes from './BlogPosts.module.css';
 
+const DEFAULT_URL = "https://jsonplaceholder.typicode.com/posts" ;
+// const DEFAULT_URL = "posts.json" ;
+
+async function fetchPosts(url) {
+    const response = await fetch(url);
+    const blogPosts = await response.json();
+    return blogPosts;
+}
+
 function BlogPosts() {
-  return <ul classes={classes.list}></ul>;
+  const [postsUrl, setPostsUrl] = useState(DEFAULT_URL);
+  const [loadedPosts, setLoadedPosts] = useState([]);
+
+  useEffect(function () {
+      console.log("fetching posts");
+      fetchPosts(postsUrl).then( (fetchedPosts) => { setLoadedPosts(fetchedPosts) }) ;
+  }, [postsUrl]);
+
+  function adjustUrlHandler(event) {
+      console.log("url", event.target.value);
+      setPostsUrl(event.target.value);
+  }
+
+  return (
+      <>
+          <ul className={classes.posts}>
+              {
+                  loadedPosts.map( post => (
+                      <li key={post.id}>{ post.title }</li>
+                  ))
+              }
+          </ul>
+      </>
+  )
 }
 
 export default BlogPosts;
