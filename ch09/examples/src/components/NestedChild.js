067da4067fae645e3b87579eb21fa37331e54fd4
David Stevenson <drstevenson1958@yahoo.com>
2024-05-01 18:20:25 -0400

    Revisiting Component Evaluations and Updates

---
+++ ch09/examples/src/components/NestedChild.js
@@ -0,0 +1,39 @@
+import { useState } from 'react' ;
+
+function NestedChild() {
+    console.log("<NestedChild /> is called");
+
+    return <p id="nested-child">A component, deeply nested into the component tree.</p>
+}
+
+function Child() {
+    console.log("<Child /> is called.");
+
+    return (
+        <div id="child">
+            <p>
+                A component, rendered inside another component, containing yet another component.
+            </p>
+            <NestedChild />
+        </div>
+    )
+}
+
+export default function Parent() {
+    console.log("<Parent /> is called.");
+
+    const [counter, setCounter] = useState(0) ;
+
+    function incCounterHandler() {
+        setCounter( (prevCounter) => prevCounter + 1 ) ;
+    }
+
+    return (
+        <div id="parent">
+            <p>A component, nested into App, containing another component (Child).</p>
+            <p>Counter: {counter}</p>
+            <button onClick={incCounterHandler}>Increment</button>
+            <Child />
+        </div>
+    )
+}
