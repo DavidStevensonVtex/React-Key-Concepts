42ca709f3f696f270ea1dfc30bb9885027bb778f
David Stevenson <drstevenson1958@yahoo.com>
2024-05-01 18:43:02 -0400

    State Batching

--- ch09/examples/src/App.js
+++ ch09/examples/src/App.js
@@ -1,9 +1,23 @@
+import { useState } from 'react' ;
 import './App.css';
-import Parent from './components/NestedChild' ;
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [showCounter, setShowCounter] = useState(false);
+
+  function incCounterHandler() {
+    setCounter( (prevCounter) => prevCounter + 1 );
+    if (!showCounter) {
+      setShowCounter(true);
+    }
+  }
+
   return (
-    <Parent />
+    <>
+      <p>Click to increment + show or hide the counter</p>
+      <button onClick={incCounterHandler}>Increment</button>
+      { showCounter && <p>Counter: {counter}</p> }
+    </>
   );
 }
 
