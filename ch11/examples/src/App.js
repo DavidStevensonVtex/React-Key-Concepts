fe89d45560bd005cdd2becd40e9151ea1b4ce573
David Stevenson <drstevenson1958@yahoo.com>
2024-05-12 12:11:39 -0400

    Load Posts button gives an error (not fixed)

--- ch11/examples/src/App.js
+++ ch11/examples/src/App.js
@@ -6,6 +6,14 @@ function App() {
     'https://jsonplaceholder.typicode.com/posts'
   );
 
+  // Load Posts button gives an error:
+
+  // ERROR
+  // Invalid hook call. Hooks can only be called inside of the body of a function component. This could happen for one of the following reasons:
+  // 1. You might have mismatching versions of React and the renderer (such as React DOM)
+  // 2. You might be breaking the Rules of Hooks
+  // 3. You might have more than one copy of React in the same app
+
   return (
     <>
       <header>
